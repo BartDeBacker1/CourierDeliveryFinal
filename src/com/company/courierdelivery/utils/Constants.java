4e437c6c6105594090c596375e98e4f0c74334f5
Bart <bart.de.backer1@student.ehb.be>
2020-08-20 12:42:23 +0200

    constants updated

--- src/com/company/courierdelivery/utils/Constants.java
+++ src/com/company/courierdelivery/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static String UNICODE = "useSSL=false&autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8";
 
     public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String DB_URL = "jdbc:mysql:/dt5.ehb.be:3306/phpmyadmin/1920progproj1_bartdebacker";
+    public static final String DB_URL = "jdbc:mysql://dt5.ehb.be:3306/1920progproj1_bartdebacker";
 
     public static final String DB_USER = "1920progproj1_bartdebacker";
     public static final String DB_PASS = "XNPU9YDNk";
